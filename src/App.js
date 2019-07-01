e5221a875b768dff4c668b0bf68b3ba8bd51bfdf
Bryan Gumpic <darlingbryan@gmail.com>
2019-06-30 20:47:22 -0400

    Set-up Marterilize Modal capability

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 
 const App = () => {
+  useEffect(() => {
+    M.AutoInit()
+  })
   return <div>App</div>
 }
 
