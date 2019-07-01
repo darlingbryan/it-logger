e5ef433ef93c55437f967488758ce8fa80844c77
Bryan Gumpic <darlingbryan@gmail.com>
2019-06-30 20:58:36 -0400

    Create SearchBar component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
+import SearchBar from './components/layout/SearchBar'
 
 const App = () => {
   useEffect(() => {
     M.AutoInit()
   })
-  return <div>App</div>
+  return (
+    <Fragment>
+      <SearchBar />
+    </Fragment>
+  )
 }
 
 export default App
